aead1b92c218c9b283e09218e3564dd67ad1cf6a
Aleksey-Shtr1h <52354908+Aleksey-Shtr1h@users.noreply.github.com>
2020-04-30 14:36:58 +0700

    module6 data binding

---
+++ src/controllers/task.js
@@ -0,0 +1,97 @@
+import EditTaskComponent from '../components/create-site-editTask.js';
+import CardTaskComponent from '../components/create-site-cardTask.js';
+
+import {renderTemplate, replaceTemplate, RenderPosition} from '../utils/render.js';
+
+const Mode = {
+  DEFAULT: `default`,
+  EDIT: `edit`,
+};
+
+const StatusCodesEsc = {
+  ESCAPE: `Escape`,
+  ESC: `Esc`,
+};
+
+export default class TaskController {
+  constructor(container, onDataChange, onViewChange) {
+    this._container = container;
+    this._onDataChange = onDataChange;
+
+    this._onViewChange = onViewChange;
+    this._mode = Mode.DEFAULT;
+
+    this._taskComponent = null;
+    this._taskEditComponent = null;
+
+    this._onEscKeyDown = this._onEscKeyDown.bind(this);
+  }
+
+  renderTask(task) {
+    const oldTaskComponent = this._taskComponent;
+    const oldTaskEditComponent = this._taskEditComponent;
+
+    this._taskComponent = new CardTaskComponent(task);
+    this._taskEditComponent = new EditTaskComponent(task);
+
+    this._taskComponent.setEditButtonClickHandler(() => {
+      this._replaceTaskToEdit();
+      document.addEventListener(`keydown`, this._onEscKeyDown);
+    });
+
+    this._taskComponent.setArchiveButtonClickHandler(() => {
+      this._onDataChange(this, task, Object.assign({}, task, {
+        isArchive: !task.isArchive,
+      }));
+    });
+
+    this._taskComponent.setFavoritesButtonClickHandler(() => {
+      this._onDataChange(this, task, Object.assign({}, task, {
+        isFavorite: !task.isFavorite,
+      }));
+    });
+
+    this._taskEditComponent.setSubmitHandler((evt) => {
+      evt.preventDefault();
+      this._replaceEditToTask();
+      document.removeEventListener(`keydown`, this._onEscKeyDown);
+    });
+
+    if (oldTaskEditComponent && oldTaskComponent) {
+      replaceTemplate(this._taskComponent, oldTaskComponent);
+      replaceTemplate(this._taskEditComponent, oldTaskEditComponent);
+    } else {
+      renderTemplate(this._container, this._taskComponent, RenderPosition.BEFOREEND);
+    }
+  }
+
+  setDefaultView() {
+    if (this._mode !== Mode.DEFAULT) {
+      this._replaceEditToTask();
+    }
+  }
+
+  _replaceTaskToEdit() {
+    this._onViewChange();
+    replaceTemplate(this._taskEditComponent, this._taskComponent);
+    this._mode = Mode.EDIT;
+  }
+
+  _replaceEditToTask() {
+    document.removeEventListener(`keydown`, this._onEscKeyDown);
+    this._taskEditComponent.reset();
+    replaceTemplate(this._taskComponent, this._taskEditComponent);
+    this._mode = Mode.DEFAULT;
+  }
+
+  _onEscKeyDown(evt) {
+    const isEscKay = evt.key === StatusCodesEsc.ESCAPE || StatusCodesEsc.ESC;
+    if (isEscKay) {
+      this._replaceEditToTask();
+      document.removeEventListener(`keydown`, this._onEscKeyDown);
+    }
+
+  }
+
+
+}
--- src/pagination.js
+++ src/pagination.js
@@ -3,11 +3,12 @@ import {remove} from './utils/render.js';
 
 export const createPaginationTasks = (tasks, loadBtn, container, sortedTasks, showingTasksCount) => {
 
-  renderTasks(container, sortedTasks);
+  const newTask = renderTasks(container, sortedTasks);
 
   if (showingTasksCount >= tasks.length) {
     remove(loadBtn);
-    loadBtn.removeElement();
   }
+
+  return newTask;
 
 };
